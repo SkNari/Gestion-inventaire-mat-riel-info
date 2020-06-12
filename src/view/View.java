ebf90f1225fe9ca76b00923b444ae973073009ec
Renox91 <renox91250@hotmail.fr>
2020-06-12 18:07:49 +0200

    Oui

--- src/view/View.java
+++ src/view/View.java
@@ -4,12 +4,29 @@ import java.util.Scanner;
 
 public class View {
     
+    private Scanner scan;
     
     public View()
     {
         
     }
 
+    public void afficherMenu()
+    {
+        System.out.println("╔═════════════ MENU ═════════════╗");
+        System.out.println("║                                ║");
+        System.out.println("║                                ║");
+        System.out.println("║                                ║");
+        System.out.println("║                                ║");
+        System.out.println("╚════════════════════════════════╝");
+    }
+
+    public String supprimer()
+    {
+        System.out.println("Saisissez l'id du materiel : ");
+        String id = scan.nextLine();
+        return id;
+    }
     
 
 
