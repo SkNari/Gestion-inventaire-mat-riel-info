482d453277b4afc370addac2a3a09b018e5f7bad
Renox91 <50325665+Renox91@users.noreply.github.com>
2020-06-12 17:04:09 +0200

    d
    d

--- src/view/View.java
+++ src/view/View.java
@@ -7,11 +7,17 @@ public class View {
     
     public View()
     {
+        
+    }
+
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         System.out.print("Quel est votre prenom ? : "+str);
-
-        System.out.print("Quel est votre nom ? : ");
+        sc = new Scanner(System.in);
+        str = sc.nextLine();
+        System.out.print("Quel est votre nom ? : "+str);
     }
 
 
